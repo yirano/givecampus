3f9a523bd6b8bc4e4d5add344f72f728722e23d8
yirano <yirano@protonmail.ch>
2020-10-13 15:00:04 -0400

    Add Header component for title and caption

--- Components/App.jsx
+++ Components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Form from "./Todos/Form"
 import List from "./Todos/List"
+import Header from './Header'
 
 const App = () => {
     return (
-        <div>
+        <div className="app">
+            <Header />
             <Form />
             <List />
         </div>
---
+++ Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>Todo</h1>
+            <p>Let's get some stuff done today!</p>
+        </div>
+    )
+}
+
+export default Header
