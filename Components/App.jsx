4565578a84d4ca54ee516e085e526e823793cc51
yirano <yirano@protonmail.ch>
2020-10-13 15:31:05 -0400

    Style header and form

--- Components/App.jsx
+++ Components/App.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Form from "./Todos/Form"
 import List from "./Todos/List"
 import Header from './Header'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
 const App = () => {
+    const [hideForm, setHideForm] = useState(false)
     return (
         <div className="app">
             <Header />
-            <Form />
+            <div className="toggleForm">
+                <button onClick={() => setHideForm(!hideForm)}>
+                    <FontAwesomeIcon icon={faPlus} />Add task
+                </button>
+                {hideForm ? '' : <Form />}
+            </div>
             <List />
         </div>
     )
--- Components/Todos/Form.jsx
+++ Components/Todos/Form.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Form = () => {
+const today = new Date()
+const date = String(today.getDate()).padStart(2, '0')
+const month = String(today.getMonth() + 1).padStart(2, '0')
+const year = today.getFullYear()
+
+const initialForm = {
+    task: '',
+    due: `${year}-${month}-${date}`,
+    completed: false,
+    id: Date.now()
+}
+
+const Form = (props) => {
+    const [form, setForm] = useState(initialForm)
+    const list = JSON.parse(localStorage.getItem('todos')) || []
+
+    const handleChange = e => {
+        setForm({ ...form, [e.target.name]: e.target.value })
+    }
+
+    const handleSubmit = () => {
+        list.push(form)
+        setForm(initialForm)
+        localStorage.setItem('todos', JSON.stringify(list))
+    }
+
     return (
-        <form>
-            <h1>Form</h1>
+        <form onSubmit={e => handleSubmit(e)}>
+            <input
+                type="text"
+                name="task"
+                value={form.task}
+                onChange={e => handleChange(e)}
+                className="taskForm"
+                placeholder="Task"
+            />
+            <input
+                type="date"
+                name="due"
+                value={form.due}
+                onChange={e => handleChange(e)}
+                className="taskForm"
+            />
+            <input type="submit" value="Add" />
         </form>
     )
 }
